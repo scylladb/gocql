smiklosovic/cassandra-gocql-driver

Identify nodes by their host_id instead broadcasted_address
Currently driver identifies nodes based on their broadcasted IP
address. In cloud case, broadcasted IP addresses are private and are not
meant to be used as a contact point, and they may change overtime.
Hence driver internals were changed to identify nodes based on their
host_id which is unique per node and it's persistant throught entire
node lifecycle.

Because CQL Events are still using broadcasted IP addresses, driver
keep mapping between already known IP addresses and host_ids.

Prepared statement cache key was also changed to host_id, to not
invalidate it upon IP change.

host_source.go
@@ -174,6 +174,22 @@ func (h *HostInfo) connectAddressLocked() (net.IP, string) {
 	return net.IPv4zero, "invalid"
 }
 
+// NativeAddress returns address broadcasted between node to nodes.
+// It's either `broadcast_address` if host info is read from system.local or `peer` if read from system.peers.
+// This IP address is also part of CQL Event emitted on topology/status changes,
+// but does not uniquely identify the node in case multiple nodes use the same IP address.
+func (h *HostInfo) NativeAddress() net.IP {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
+	if validIpAddr(h.broadcastAddress) {
+		return h.broadcastAddress
+	} else if validIpAddr(h.peer) {
+		return h.peer
+	}
+	return net.IPv4zero
+}
+
 // Returns the address that should be used to connect to the host.
 // If you wish to override this, use an AddressTranslator or
 // use a HostFilter to SetConnectAddress()
@@ -237,6 +253,12 @@ func (h *HostInfo) HostID() string {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
 	return h.hostId
+}
+
+func (h *HostInfo) SetHostID(hostID string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.hostId = hostID
 }
 
 func (h *HostInfo) WorkLoad() string {
@@ -583,44 +605,40 @@ func (r *ringDescriber) GetHosts() ([]*HostInfo, string, error) {
 }
 
 // Given an ip/port return HostInfo for the specified ip/port
-func (r *ringDescriber) getHostInfo(ip net.IP, port int) (*HostInfo, error) {
+func (r *ringDescriber) getHostInfo(hostID UUID) (*HostInfo, error) {
 	var host *HostInfo
-	iter := r.session.control.withConnHost(func(ch *connHost) *Iter {
-		if ch.host.ConnectAddress().Equal(ip) {
-			host = ch.host
-			return nil
-		}
+	for _, table := range []string{"system.peers", "system.local"} {
+		iter := r.session.control.withConnHost(func(ch *connHost) *Iter {
+			if ch.host.HostID() == hostID.String() {
+				host = ch.host
+				return nil
+			}
 
-		return ch.conn.query(context.TODO(), "SELECT * FROM system.peers")
-	})
+			return ch.conn.query(context.TODO(), fmt.Sprintf("SELECT * FROM %s", table))
+		})
 
-	if iter != nil {
-		rows, err := iter.SliceMap()
-		if err != nil {
-			return nil, err
-		}
-
-		for _, row := range rows {
-			h, err := r.session.hostInfoFromMap(row, &HostInfo{port: port})
+		if iter != nil {
+			rows, err := iter.SliceMap()
 			if err != nil {
 				return nil, err
 			}
 
-			if h.ConnectAddress().Equal(ip) {
-				host = h
-				break
-			}
-		}
+			for _, row := range rows {
+				h, err := r.session.hostInfoFromMap(row, &HostInfo{port: r.session.cfg.Port})
+				if err != nil {
+					return nil, err
+				}
 
-		if host == nil {
-			return nil, errors.New("host not found in peers table")
+				if h.HostID() == hostID.String() {
+					host = h
+					break
+				}
+			}
 		}
 	}
 
 	if host == nil {
 		return nil, errors.New("unable to fetch host info: invalid control connection")
-	} else if host.invalidConnectAddr() {
-		return nil, fmt.Errorf("host ConnectAddress invalid ip=%v: %v", ip, host)
 	}
 
 	return host, nil
@@ -649,7 +667,7 @@ func (r *ringDescriber) refreshRing() error {
 		} else {
 			host.update(h)
 		}
-		delete(prevHosts, h.ConnectAddress().String())
+		delete(prevHosts, h.HostID())
 	}
 
 	// TODO(zariel): it may be worth having a mutex covering the overall ring state

ring.go
@@ -2,7 +2,6 @@ package gocql
 
 import (
 	"fmt"
-	"net"
 	"sync"
 	"sync/atomic"
 )
@@ -13,9 +12,12 @@ type ring struct {
 	// strap the initial connection.
 	endpoints []*HostInfo
 
-	// hosts are the set of all hosts in the cassandra ring that we know of
-	mu    sync.RWMutex
+	mu sync.RWMutex
+	// hosts are the set of all hosts in the cassandra ring that we know of.
+	// key of map is host_id.
 	hosts map[string]*HostInfo
+	// hostIPToUUID maps host native address to host_id.
+	hostIPToUUID map[string]string
 
 	hostList []*HostInfo
 	pos      uint32
@@ -34,9 +36,16 @@ func (r *ring) rrHost() *HostInfo {
 	return r.hostList[pos%len(r.hostList)]
 }
 
-func (r *ring) getHost(ip net.IP) *HostInfo {
+func (r *ring) getHostByIP(ip string) (*HostInfo, bool) {
 	r.mu.RLock()
-	host := r.hosts[ip.String()]
+	defer r.mu.RUnlock()
+	hi, ok := r.hostIPToUUID[ip]
+	return r.hosts[hi], ok
+}
+
+func (r *ring) getHost(hostID string) *HostInfo {
+	r.mu.RLock()
+	host := r.hosts[hostID]
 	r.mu.RUnlock()
 	return host
 }
@@ -73,16 +82,20 @@ func (r *ring) addHostIfMissing(host *HostInfo) (*HostInfo, bool) {
 	if host.invalidConnectAddr() {
 		panic(fmt.Sprintf("invalid host: %v", host))
 	}
-	ip := host.ConnectAddress().String()
+	hostID := host.HostID()
 
 	r.mu.Lock()
 	if r.hosts == nil {
 		r.hosts = make(map[string]*HostInfo)
 	}
+	if r.hostIPToUUID == nil {
+		r.hostIPToUUID = make(map[string]string)
+	}
 
-	existing, ok := r.hosts[ip]
+	existing, ok := r.hosts[hostID]
 	if !ok {
-		r.hosts[ip] = host
+		r.hosts[hostID] = host
+		r.hostIPToUUID[host.NativeAddress().String()] = hostID
 		existing = host
 		r.hostList = append(r.hostList, host)
 	}
@@ -90,23 +103,26 @@ func (r *ring) addHostIfMissing(host *HostInfo) (*HostInfo, bool) {
 	return existing, ok
 }
 
-func (r *ring) removeHost(ip net.IP) bool {
+func (r *ring) removeHost(hostID string) bool {
 	r.mu.Lock()
 	if r.hosts == nil {
 		r.hosts = make(map[string]*HostInfo)
 	}
+	if r.hostIPToUUID == nil {
+		r.hostIPToUUID = make(map[string]string)
+	}
 
-	k := ip.String()
-	_, ok := r.hosts[k]
+	h, ok := r.hosts[hostID]
 	if ok {
 		for i, host := range r.hostList {
-			if host.ConnectAddress().Equal(ip) {
+			if host.HostID() == hostID {
 				r.hostList = append(r.hostList[:i], r.hostList[i+1:]...)
 				break
 			}
 		}
+		delete(r.hostIPToUUID, h.NativeAddress().String())
 	}
-	delete(r.hosts, k)
+	delete(r.hosts, hostID)
 	r.mu.Unlock()
 	return ok
 }

scylladb/gocql

host_source.go
@@ -178,6 +178,22 @@ func (h *HostInfo) connectAddressLocked() (net.IP, string) {
 	return net.IPv4zero, "invalid"
 }
 
+// nodeToNodeAddress returns address broadcasted between node to nodes.
+// It's either `broadcast_address` if host info is read from system.local or `peer` if read from system.peers.
+// This IP address is also part of CQL Event emitted on topology/status changes,
+// but does not uniquely identify the node in case multiple nodes use the same IP address.
+func (h *HostInfo) nodeToNodeAddress() net.IP {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
+	if validIpAddr(h.broadcastAddress) {
+		return h.broadcastAddress
+	} else if validIpAddr(h.peer) {
+		return h.peer
+	}
+	return net.IPv4zero
+}
+
 // Returns the address that should be used to connect to the host.
 // If you wish to override this, use an AddressTranslator or
 // use a HostFilter to SetConnectAddress()
@@ -255,6 +271,12 @@ func (h *HostInfo) HostID() string {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
 	return h.hostId
+}
+
+func (h *HostInfo) SetHostID(hostID string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.hostId = hostID
 }
 
 func (h *HostInfo) WorkLoad() string {
@@ -628,44 +650,40 @@ func (r *ringDescriber) GetHosts() ([]*HostInfo, string, error) {
 }
 
 // Given an ip/port return HostInfo for the specified ip/port
-func (r *ringDescriber) getHostInfo(ip net.IP, port int) (*HostInfo, error) {
+func (r *ringDescriber) getHostInfo(hostID UUID) (*HostInfo, error) {
 	var host *HostInfo
-	iter := r.session.control.withConnHost(func(ch *connHost) *Iter {
-		if ch.host.ConnectAddress().Equal(ip) {
-			host = ch.host
-			return nil
-		}
+	for _, table := range []string{"system.peers", "system.local"} {
+		iter := r.session.control.withConnHost(func(ch *connHost) *Iter {
+			if ch.host.HostID() == hostID.String() {
+				host = ch.host
+				return nil
+			}
 
-		return ch.conn.query(context.TODO(), "SELECT * FROM system.peers")
-	})
+			return ch.conn.query(context.TODO(), fmt.Sprintf("SELECT * FROM %s", table))
+		})
 
-	if iter != nil {
-		rows, err := iter.SliceMap()
-		if err != nil {
-			return nil, err
-		}
-
-		for _, row := range rows {
-			h, err := r.session.hostInfoFromMap(row, &HostInfo{port: port})
+		if iter != nil {
+			rows, err := iter.SliceMap()
 			if err != nil {
 				return nil, err
 			}
 
-			if h.ConnectAddress().Equal(ip) {
-				host = h
-				break
-			}
-		}
+			for _, row := range rows {
+				h, err := r.session.hostInfoFromMap(row, &HostInfo{port: r.session.cfg.Port})
+				if err != nil {
+					return nil, err
+				}
 
-		if host == nil {
-			return nil, errors.New("host not found in peers table")
+				if h.HostID() == hostID.String() {
+					host = h
+					break
+				}
+			}
 		}
 	}
 
 	if host == nil {
 		return nil, errors.New("unable to fetch host info: invalid control connection")
-	} else if host.invalidConnectAddr() {
-		return nil, fmt.Errorf("host ConnectAddress invalid ip=%v: %v", ip, host)
 	}
 
 	return host, nil
@@ -694,7 +712,7 @@ func (r *ringDescriber) refreshRing() error {
 		} else {
 			host.update(h)
 		}
-		delete(prevHosts, h.ConnectAddress().String())
+		delete(prevHosts, h.HostID())
 	}
 
 	// TODO(zariel): it may be worth having a mutex covering the overall ring state

ring.go
@@ -2,7 +2,6 @@ package gocql
 
 import (
 	"fmt"
-	"net"
 	"sync"
 	"sync/atomic"
 )
@@ -13,9 +12,12 @@ type ring struct {
 	// strap the initial connection.
 	endpoints []*HostInfo
 
-	// hosts are the set of all hosts in the cassandra ring that we know of
-	mu    sync.RWMutex
+	mu sync.RWMutex
+	// hosts are the set of all hosts in the cassandra ring that we know of.
+	// key of map is host_id.
 	hosts map[string]*HostInfo
+	// hostIPToUUID maps host native address to host_id.
+	hostIPToUUID map[string]string
 
 	hostList []*HostInfo
 	pos      uint32
@@ -36,9 +38,16 @@ func (r *ring) rrHost() *HostInfo {
 	return r.hostList[pos%len(r.hostList)]
 }
 
-func (r *ring) getHost(ip net.IP) *HostInfo {
+func (r *ring) getHostByIP(ip string) (*HostInfo, bool) {
 	r.mu.RLock()
-	host := r.hosts[ip.String()]
+	defer r.mu.RUnlock()
+	hi, ok := r.hostIPToUUID[ip]
+	return r.hosts[hi], ok
+}
+
+func (r *ring) getHost(hostID string) *HostInfo {
+	r.mu.RLock()
+	host := r.hosts[hostID]
 	r.mu.RUnlock()
 	return host
 }
@@ -75,16 +84,20 @@ func (r *ring) addHostIfMissing(host *HostInfo) (*HostInfo, bool) {
 	if host.invalidConnectAddr() {
 		panic(fmt.Sprintf("invalid host: %v", host))
 	}
-	ip := host.ConnectAddress().String()
+	hostID := host.HostID()
 
 	r.mu.Lock()
 	if r.hosts == nil {
 		r.hosts = make(map[string]*HostInfo)
 	}
+	if r.hostIPToUUID == nil {
+		r.hostIPToUUID = make(map[string]string)
+	}
 
-	existing, ok := r.hosts[ip]
+	existing, ok := r.hosts[hostID]
 	if !ok {
-		r.hosts[ip] = host
+		r.hosts[hostID] = host
+		r.hostIPToUUID[host.nodeToNodeAddress().String()] = hostID
 		existing = host
 		r.hostList = append(r.hostList, host)
 	}
@@ -92,23 +105,26 @@ func (r *ring) addHostIfMissing(host *HostInfo) (*HostInfo, bool) {
 	return existing, ok
 }
 
-func (r *ring) removeHost(ip net.IP) bool {
+func (r *ring) removeHost(hostID string) bool {
 	r.mu.Lock()
 	if r.hosts == nil {
 		r.hosts = make(map[string]*HostInfo)
 	}
+	if r.hostIPToUUID == nil {
+		r.hostIPToUUID = make(map[string]string)
+	}
 
-	k := ip.String()
-	_, ok := r.hosts[k]
+	h, ok := r.hosts[hostID]
 	if ok {
 		for i, host := range r.hostList {
-			if host.ConnectAddress().Equal(ip) {
+			if host.HostID() == hostID {
 				r.hostList = append(r.hostList[:i], r.hostList[i+1:]...)
 				break
 			}
 		}
+		delete(r.hostIPToUUID, h.nodeToNodeAddress().String())
 	}
-	delete(r.hosts, k)
+	delete(r.hosts, hostID)
 	r.mu.Unlock()
 	return ok
 }