scylladb/gocql

commit c6477e2efaf9c9003487f73b71f9c975c41e9a49

Stop translating initial endpoints

scylla.go
@@ -730,8 +730,10 @@ func (sd *scyllaDialer) DialShard(ctx context.Context, host *HostInfo, shardID, 
 	translatedInfo := host.getTranslatedConnectionInfo()
 	if translatedInfo != nil {
 		addr = translatedInfo.CQL.ToNetAddr()
-		if sd.tlsConfig != nil && translatedInfo.ShardAwareTLS.IsValid() {
-			shardAwareAddr = translatedInfo.ShardAwareTLS.ToNetAddr()
+		if sd.tlsConfig != nil {
+			if translatedInfo.ShardAwareTLS.IsValid() {
+				shardAwareAddr = translatedInfo.ShardAwareTLS.ToNetAddr()
+			}
 		} else if translatedInfo.ShardAware.IsValid() {
 			shardAwareAddr = translatedInfo.ShardAware.ToNetAddr()
 		}

conn_test.go
@@ -62,7 +62,12 @@ const (
 type brokenDNSResolver struct{}
 
 func (b brokenDNSResolver) LookupIP(host string) ([]net.IP, error) {
-	return nil, &net.DNSError{}
+	err := errors.New("this error comes from mocked broken resolver")
+	return nil, &net.DNSError{
+		UnwrapErr: err,
+		Err:       err.Error(),
+		Server:    host,
+	}
 }
 
 func TestApprove(t *testing.T) {
@@ -202,8 +207,8 @@ func TestDNSLookupConnected(t *testing.T) {
 		t.Fatal("CreateSession() should have connected")
 	}
 
-	if !strings.Contains(log.String(), "gocql: dns error") {
-		t.Fatalf("Expected to receive dns error log message  - got '%s' instead", log.String())
+	if !strings.Contains(log.String(), "failed to resolve endpoint") {
+		t.Fatalf("Expected to receive 'failed to resolve endpoint' log message  - got '%s' instead", log.String())
 	}
 }
 
@@ -211,8 +216,9 @@ func TestDNSLookupError(t *testing.T) {
 	log := &testLogger{}
 
 	// Override the defaul DNS resolver and restore at the end
+	hosts := []string{"cassandra1.invalid", "cassandra2.invalid"}
 
-	cluster := NewCluster("cassandra1.invalid", "cassandra2.invalid")
+	cluster := NewCluster(hosts...)
 	cluster.Logger = log
 	cluster.ProtoVersion = int(defaultProto)
 	cluster.disableControlConn = true
@@ -225,12 +231,19 @@ func TestDNSLookupError(t *testing.T) {
 		t.Fatal("CreateSession() should have returned an error")
 	}
 
-	if !strings.Contains(log.String(), "gocql: dns error") {
-		t.Fatalf("Expected to receive dns error log message  - got '%s' instead", log.String())
+	if !strings.Contains(log.String(), "failed to resolve endpoint") {
+		t.Fatalf("Expected to receive 'failed to resolve endpoint' log message  - got '%s' instead", log.String())
 	}
 
-	if err.Error() != "gocql: unable to create session: failed to resolve any of the provided hostnames" {
-		t.Fatalf("Expected CreateSession() to fail with message  - got '%s' instead", err.Error())
+	if !strings.Contains(err.Error(), "unable to create session: failed to resolve any of the provided hostnames") {
+		t.Fatalf("Expected CreateSession() to fail with error message that contains 'unable to create session: failed to resolve any of the provided hostnames'")
+	}
+
+	for _, host := range hosts {
+		expected := fmt.Sprintf("failed to resolve endpoint \"%s\": lookup  on %s: this error comes from mocked broken resolver", host, host)
+		if !strings.Contains(err.Error(), expected) {
+			t.Fatalf("Expected to fail with error message that contains '%s'", expected)
+		}
 	}
 }
 
@@ -1248,7 +1261,7 @@ func (srv *TestServer) session() (*Session, error) {
 }
 
 func (srv *TestServer) host() *HostInfo {
-	hosts, err := translateAndResolveInitialEndpoint(nil, nil, srv.Address, 9042)
+	hosts, err := resolveInitialEndpoint(nil, srv.Address, 9042)
 	if err != nil {
 		srv.t.Fatal(err)
 	}

control.go
@@ -144,7 +144,7 @@ func (c *controlConn) heartBeat() {
 	}
 }
 
-func translateAndResolveInitialEndpoint(resolver DNSResolver, translateAddressPort addressTranslateFn, addr string, defaultPort int) ([]*HostInfo, error) {
+func resolveInitialEndpoint(resolver DNSResolver, addr string, defaultPort int) ([]*HostInfo, error) {
 	var port int
 	host, portStr, err := net.SplitHostPort(addr)
 	if err != nil {
@@ -165,19 +165,6 @@ func translateAndResolveInitialEndpoint(resolver DNSResolver, translateAddressPo
 				Hostname:       host,
 				ConnectAddress: ip,
 				Port:           port,
-				TranslatedAddresses: &translatedAddresses{
-					CQL: AddressPort{
-						Address: ip,
-						Port:    uint16(port),
-					},
-				},
-			}
-			if translateAddressPort != nil {
-				// empty hostID signals that it is an initial contact endpoint
-				hb.TranslatedAddresses.CQL = translateAddressPort("", AddressPort{
-					Address: ip,
-					Port:    uint16(port),
-				})
 			}
 			hh := hb.Build()
 			return []*HostInfo{&hh}, nil
@@ -200,19 +187,6 @@ func translateAndResolveInitialEndpoint(resolver DNSResolver, translateAddressPo
 				Hostname:       host,
 				ConnectAddress: ip,
 				Port:           port,
-				TranslatedAddresses: &translatedAddresses{
-					CQL: AddressPort{
-						Address: ip,
-						Port:    uint16(port),
-					},
-				},
-			}
-			if translateAddressPort != nil {
-				// empty hostID signals that it is an initial contact endpoint
-				hb.TranslatedAddresses.CQL = translateAddressPort("", AddressPort{
-					Address: ip,
-					Port:    uint16(port),
-				})
 			}
 			hh := hb.Build()
 			hosts = append(hosts, &hh)
@@ -476,7 +450,7 @@ func (c *controlConn) attemptReconnect() error {
 	c.session.logger.Printf("gocql: control falling back to initial contact points.\n")
 	// Fallback to initial contact points, as it may be the case that all known initialHosts
 	// changed their IPs while keeping the same hostname(s).
-	initialHosts, resolvErr := translateAndResolveInitialEndpoints(c.session.cfg.DNSResolver, c.session.cfg.translateAddressPort, c.session.cfg.Hosts, c.session.cfg.Port, c.session.logger)
+	initialHosts, resolvErr := resolveInitialEndpoints(c.session.cfg.DNSResolver, c.session.cfg.Hosts, c.session.cfg.Port, c.session.logger)
 	if resolvErr != nil {
 		return fmt.Errorf("resolve contact points' hostnames: %v", resolvErr)
 	}

control_test.go
@@ -48,7 +48,7 @@ func TestHostInfo_Lookup(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		hosts, err := translateAndResolveInitialEndpoint(resolver, nil, test.addr, 1)
+		hosts, err := resolveInitialEndpoint(resolver, test.addr, 1)
 		if err != nil {
 			t.Errorf("%d: %v", i, err)
 			continue

exec.go
@@ -69,7 +69,7 @@ func NewSingleHostQueryExecutor(cfg *ClusterConfig) (e SingleHostQueryExecutor, 
 	}
 
 	var hosts []*HostInfo
-	if hosts, err = translateAndResolveInitialEndpoints(c.DNSResolver, c.translateAddressPort, c.Hosts, c.Port, c.Logger); err != nil {
+	if hosts, err = resolveInitialEndpoints(c.DNSResolver, c.Hosts, c.Port, c.Logger); err != nil {
 		err = fmt.Errorf("addrs to hosts: %w", err)
 		return
 	}

filters.go
@@ -70,7 +70,7 @@ func DataCentreHostFilter(dataCenter string) HostFilter {
 // WhiteListHostFilter filters incoming hosts by checking that their address is
 // in the initial hosts whitelist.
 func WhiteListHostFilter(hosts ...string) HostFilter {
-	hostInfos, err := translateAndResolveInitialEndpoints(defaultDnsResolver, nil, hosts, 9042, nopLogger{})
+	hostInfos, err := resolveInitialEndpoints(defaultDnsResolver, hosts, 9042, nopLogger{})
 	if err != nil {
 		// dont want to panic here, but rather not break the API
 		panic(fmt.Errorf("unable to lookup host info from address: %v", err))

host_source.go
@@ -162,7 +162,7 @@ func (a *AddressPort) Equal(o AddressPort) bool {
 }
 
 func (a *AddressPort) IsValid() bool {
-	return a.Address != nil && !a.Address.IsUnspecified() && a.Port != 0
+	return len(a.Address) != 0 && !a.Address.IsUnspecified() && a.Port != 0
 }
 
 func (a *AddressPort) String() string {

scylla_shard_aware_port_common_test.go
@@ -62,7 +62,7 @@ func testShardAwarePortNoReconnections(t *testing.T, makeCluster makeClusterTest
 
 			hosts := sess.hostSource.getHostsList()
 			for _, host := range hosts {
-				t.Logf("checking host %q hostID: %q", host.hostname, host.hostId)
+				t.Logf("checking host %s:%d hostID: %q", host.ConnectAddress(), host.Port(), host.hostId)
 				hostPool, ok := sess.pool.getPool(host)
 				if !ok {
 					pushErr(fmt.Errorf("host %q hostID not found", host.hostname))

commit c84dcf402109e8acd2c70332441054d1e2099e6f

Translate address for non-shard-aware connection
Regression was introduced in https://github.com/scylladb/gocql/pull/671
Address translation was implemented only for DialShard.
This change adds it for DialHost

dial.go
@@ -71,6 +71,11 @@ func (hd *defaultHostDialer) DialHost(ctx context.Context, host *HostInfo) (*Dia
 	}
 
 	addr := net.JoinHostPort(ip.String(), strconv.Itoa(port))
+	translatedInfo := host.getTranslatedConnectionInfo()
+	if translatedInfo != nil {
+		addr = translatedInfo.CQL.ToNetAddr()
+	}
+
 	conn, err := hd.dialer.DialContext(ctx, "tcp", addr)
 	if err != nil {
 		return nil, err

scylla.go
@@ -783,6 +783,11 @@ func (sd *scyllaDialer) DialHost(ctx context.Context, host *HostInfo) (*DialedHo
 	}
 
 	addr := net.JoinHostPort(ip.String(), strconv.Itoa(port))
+	translatedInfo := host.getTranslatedConnectionInfo()
+	if translatedInfo != nil {
+		addr = translatedInfo.CQL.ToNetAddr()
+	}
+
 	conn, err := sd.dialer.DialContext(ctx, "tcp", addr)
 	if err != nil {
 		return nil, err