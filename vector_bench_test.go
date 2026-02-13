e6758415e69f39ec6e5bf88bcea40325809ff85a
Yaniv Michael Kaul <yaniv.kaul@scylladb.com>
2026-02-13 21:11:47 +0200

    bench: add vector benchmarks
    Added both unit tests and more comprehensive benchmark tests that use the driver public API
    for vector types. This will be used later to measure improved performance in future changes.
    
    Example run and baseline:
    ykaul@ykaul:~/github/gocql$ go test -tags unit -bench=BenchmarkMarshalVectorFloat32 -benchmem -cpu=1 -run=^$
    goos: linux
    goarch: amd64
    pkg: github.com/gocql/gocql
    cpu: 12th Gen Intel(R) Core(TM) i7-1270P
    BenchmarkMarshalVectorFloat32/dim_128         	  148831	      7795 ns/op	  65.68 MB/s	    2008 B/op	     261 allocs/op
    BenchmarkMarshalVectorFloat32/dim_384         	   68076	     15657 ns/op	  98.10 MB/s	    7128 B/op	     775 allocs/op
    BenchmarkMarshalVectorFloat32/dim_768         	   38354	     32523 ns/op	  94.46 MB/s	   14296 B/op	    1544 allocs/op
    BenchmarkMarshalVectorFloat32/dim_1536        	   19028	     62560 ns/op	  98.21 MB/s	   28632 B/op	    3081 allocs/op
    
    ykaul@ykaul:~/github/gocql$ go test -tags unit -bench=BenchmarkVector -benchmem -cpu=1 -run=^$
    goos: linux
    goarch: amd64
    pkg: github.com/gocql/gocql
    cpu: 12th Gen Intel(R) Core(TM) i7-1270P
    BenchmarkVectorRoundTrip/dim_128         	  105081	     11484 ns/op	  89.17 MB/s	    2544 B/op	     263 allocs/op
    BenchmarkVectorRoundTrip/dim_384         	   42028	     28541 ns/op	 107.64 MB/s	    8688 B/op	     777 allocs/op
    BenchmarkVectorRoundTrip/dim_768         	   19881	     58518 ns/op	 104.99 MB/s	   17392 B/op	    1546 allocs/op
    BenchmarkVectorRoundTrip/dim_1536        	   10000	    122615 ns/op	 100.22 MB/s	   34800 B/op	    3083 allocs/op
    
    ykaul@ykaul:~/github/gocql/tests/bench$ go test -bench=BenchmarkVector.*Public -benchmem -cpu=1 -run=^$
    goos: linux
    goarch: amd64
    pkg: github.com/gocql/gocql/bench_test
    cpu: 12th Gen Intel(R) Core(TM) i7-1270P
    BenchmarkVectorMarshalFloat32Public/dim_128         	  178554	      7316 ns/op	  69.99 MB/s	    2072 B/op	     262 allocs/op
    BenchmarkVectorMarshalFloat32Public/dim_384         	   67886	     16137 ns/op	  95.18 MB/s	    7192 B/op	     776 allocs/op
    BenchmarkVectorMarshalFloat32Public/dim_768         	   33325	     32918 ns/op	  93.32 MB/s	   14360 B/op	    1545 allocs/op
    BenchmarkVectorMarshalFloat32Public/dim_1536        	   17396	     68463 ns/op	  89.74 MB/s	   28696 B/op	    3082 allocs/op
    BenchmarkVectorUnmarshalFloat32Public/dim_128       	  280303	      4116 ns/op	 124.40 MB/s	     600 B/op	       3 allocs/op
    BenchmarkVectorUnmarshalFloat32Public/dim_384       	   99222	     11701 ns/op	 131.27 MB/s	    1624 B/op	       3 allocs/op
    BenchmarkVectorUnmarshalFloat32Public/dim_768       	   51583	     25839 ns/op	 118.89 MB/s	    3160 B/op	       3 allocs/op
    BenchmarkVectorUnmarshalFloat32Public/dim_1536      	   24544	     47639 ns/op	 128.97 MB/s	    6232 B/op	       3 allocs/op
    BenchmarkVectorRoundTripPublic/dim_128              	  112818	     10312 ns/op	  99.30 MB/s	    2672 B/op	     265 allocs/op
    BenchmarkVectorRoundTripPublic/dim_384              	   39496	     29642 ns/op	 103.64 MB/s	    8816 B/op	     779 allocs/op
    BenchmarkVectorRoundTripPublic/dim_768              	   19656	     59505 ns/op	 103.25 MB/s	   17520 B/op	    1548 allocs/op
    BenchmarkVectorRoundTripPublic/dim_1536             	    9580	    116155 ns/op	 105.79 MB/s	   34928 B/op	    3085 allocs/op
    
    Signed-off-by: Yaniv Kaul <yaniv.kaul@scylladb.com>

---
+++ vector_bench_test.go
@@ -0,0 +1,148 @@
+// Copyright (c) 2012 The gocql Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build all || unit
+// +build all unit
+
+package gocql
+
+import (
+	"encoding/binary"
+	"math"
+	"testing"
+)
+
+const vectorTypePrefix = apacheCassandraTypePrefix + "VectorType(" + apacheCassandraTypePrefix + "FloatType, "
+const vectorTypeSuffix = ")"
+
+func makeFloatVectorType(dim int, dimStr string) VectorType {
+	return VectorType{
+		NativeType: NativeType{
+			proto:  protoVersion4,
+			typ:    TypeCustom,
+			custom: vectorTypePrefix + dimStr + vectorTypeSuffix,
+		},
+		SubType:    NativeType{proto: protoVersion4, typ: TypeFloat},
+		Dimensions: dim,
+	}
+}
+
+// BenchmarkUnmarshalVectorFloat32 measures unmarshal performance for float32 vectors
+// across common embedding dimensions used in AI/ML applications.
+func BenchmarkUnmarshalVectorFloat32(b *testing.B) {
+	dims := []struct {
+		dim    int
+		dimStr string
+	}{
+		{dim: 128, dimStr: "128"},
+		{dim: 384, dimStr: "384"},
+		{dim: 768, dimStr: "768"},
+		{dim: 1536, dimStr: "1536"},
+	}
+
+	for _, entry := range dims {
+		dim := entry.dim
+		dimStr := entry.dimStr
+		b.Run("dim-"+dimStr, func(b *testing.B) {
+			b.ReportAllocs()
+
+			data := make([]byte, dim*4)
+			for i := 0; i < dim; i++ {
+				binary.BigEndian.PutUint32(data[i*4:], math.Float32bits(float32(i)*0.1))
+			}
+
+			info := makeFloatVectorType(dim, dimStr)
+			var result []float32
+
+			b.SetBytes(int64(dim * 4))
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				if err := unmarshalVector(info, data, &result); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+// BenchmarkMarshalVectorFloat32 measures marshal performance for float32 vectors
+// across common embedding dimensions.
+func BenchmarkMarshalVectorFloat32(b *testing.B) {
+	dims := []struct {
+		dim    int
+		dimStr string
+	}{
+		{dim: 128, dimStr: "128"},
+		{dim: 384, dimStr: "384"},
+		{dim: 768, dimStr: "768"},
+		{dim: 1536, dimStr: "1536"},
+	}
+
+	for _, entry := range dims {
+		dim := entry.dim
+		dimStr := entry.dimStr
+		b.Run("dim_"+dimStr, func(b *testing.B) {
+			b.ReportAllocs()
+
+			vec := make([]float32, dim)
+			for i := range vec {
+				vec[i] = float32(i) * 0.1
+			}
+
+			info := makeFloatVectorType(dim, dimStr)
+
+			b.SetBytes(int64(dim * 4))
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				if _, err := marshalVector(info, vec); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+// BenchmarkVectorRoundTrip measures full marshal -> unmarshal cycle.
+func BenchmarkVectorRoundTrip(b *testing.B) {
+	dims := []struct {
+		dim    int
+		dimStr string
+	}{
+		{dim: 128, dimStr: "128"},
+		{dim: 384, dimStr: "384"},
+		{dim: 768, dimStr: "768"},
+		{dim: 1536, dimStr: "1536"},
+	}
+
+	for _, entry := range dims {
+		dim := entry.dim
+		dimStr := entry.dimStr
+		b.Run("dim_"+dimStr, func(b *testing.B) {
+			b.ReportAllocs()
+
+			srcVec := make([]float32, dim)
+			for i := range srcVec {
+				srcVec[i] = float32(i) * 0.1
+			}
+
+			info := makeFloatVectorType(dim, dimStr)
+			var dstVec []float32
+
+			b.SetBytes(int64(dim * 4 * 2))
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				data, err := marshalVector(info, srcVec)
+				if err != nil {
+					b.Fatal(err)
+				}
+				if err := unmarshalVector(info, data, &dstVec); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
