scylladb/gocql

commit c7ad64d7a6b36d6159ee81c9d72e2ba338b25951

Make tests pass with vector support

vector_test.go
@@ -29,12 +29,13 @@ package gocql
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/require"
-	"gopkg.in/inf.v0"
 	"net"
 	"reflect"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/inf.v0"
 )
 
 type person struct {
@@ -345,9 +346,9 @@ func TestVector_SubTypeParsing(t *testing.T) {
 			name:   "vector_vector_inet",
 			custom: "org.apache.cassandra.db.marshal.VectorType(org.apache.cassandra.db.marshal.VectorType(org.apache.cassandra.db.marshal.InetAddressType, 2), 3)",
 			expected: VectorType{
-				NativeType{typ: TypeCustom, custom: VECTOR_TYPE},
+				NativeType{typ: TypeCustom, custom: "org.apache.cassandra.db.marshal.VectorType"},
 				VectorType{
-					NativeType{typ: TypeCustom, custom: VECTOR_TYPE},
+					NativeType{typ: TypeCustom, custom: "org.apache.cassandra.db.marshal.VectorType"},
 					NativeType{typ: TypeInet},
 					2,
 				},
@@ -361,7 +362,7 @@ func TestVector_SubTypeParsing(t *testing.T) {
 				NativeType{typ: TypeMap},
 				NativeType{typ: TypeInt},
 				VectorType{
-					NativeType{typ: TypeCustom, custom: VECTOR_TYPE},
+					NativeType{typ: TypeCustom, custom: "org.apache.cassandra.db.marshal.VectorType"},
 					NativeType{typ: TypeVarchar},
 					10,
 				},
@@ -376,7 +377,7 @@ func TestVector_SubTypeParsing(t *testing.T) {
 				CollectionType{
 					NativeType{typ: TypeMap},
 					VectorType{
-						NativeType{typ: TypeCustom, custom: VECTOR_TYPE},
+						NativeType{typ: TypeCustom, custom: "org.apache.cassandra.db.marshal.VectorType"},
 						NativeType{typ: TypeInt},
 						10,
 					},

frame.go
@@ -915,8 +915,8 @@ func (f *framer) readTypeInfo() TypeInfo {
 
 		return collection
 	case TypeCustom:
-		if strings.HasPrefix(simple.custom, VECTOR_TYPE) {
-			spec := strings.TrimPrefix(simple.custom, VECTOR_TYPE)
+		if strings.HasPrefix(simple.custom, "org.apache.cassandra.db.marshal.VectorType") {
+			spec := strings.TrimPrefix(simple.custom, "org.apache.cassandra.db.marshal.VectorType")
 			spec = spec[1 : len(spec)-1] // remove parenthesis
 			idx := strings.LastIndex(spec, ",")
 			typeStr := spec[:idx]

helpers.go
@@ -166,18 +166,18 @@ func getCassandraBaseType(name string) Type {
 // TODO: Cover with unit tests.
 // Parses long Java-style type definition to internal data structures.
 func getCassandraLongType(name string, protoVer byte, logger StdLogger) TypeInfo {
-	if strings.HasPrefix(name, SET_TYPE) {
+	if strings.HasPrefix(name, "org.apache.cassandra.db.marshal.SetType") {
 		return CollectionType{
 			NativeType: NewNativeType(protoVer, TypeSet),
-			Elem:       getCassandraLongType(unwrapCompositeTypeDefinition(name, SET_TYPE, '('), protoVer, logger),
+			Elem:       getCassandraLongType(unwrapCompositeTypeDefinition(name, "org.apache.cassandra.db.marshal.SetType", '('), protoVer, logger),
 		}
-	} else if strings.HasPrefix(name, LIST_TYPE) {
+	} else if strings.HasPrefix(name, "org.apache.cassandra.db.marshal.ListType") {
 		return CollectionType{
 			NativeType: NewNativeType(protoVer, TypeList),
-			Elem:       getCassandraLongType(unwrapCompositeTypeDefinition(name, LIST_TYPE, '('), protoVer, logger),
+			Elem:       getCassandraLongType(unwrapCompositeTypeDefinition(name, "org.apache.cassandra.db.marshal.ListType", '('), protoVer, logger),
 		}
-	} else if strings.HasPrefix(name, MAP_TYPE) {
-		names := splitJavaCompositeTypes(name, MAP_TYPE)
+	} else if strings.HasPrefix(name, "org.apache.cassandra.db.marshal.MapType") {
+		names := splitJavaCompositeTypes(name, "org.apache.cassandra.db.marshal.MapType")
 		if len(names) != 2 {
 			logger.Printf("gocql: error parsing map type, it has %d subelements, expecting 2\n", len(names))
 			return NewNativeType(protoVer, TypeCustom)
@@ -187,8 +187,8 @@ func getCassandraLongType(name string, protoVer byte, logger StdLogger) TypeInfo
 			Key:        getCassandraLongType(names[0], protoVer, logger),
 			Elem:       getCassandraLongType(names[1], protoVer, logger),
 		}
-	} else if strings.HasPrefix(name, TUPLE_TYPE) {
-		names := splitJavaCompositeTypes(name, TUPLE_TYPE)
+	} else if strings.HasPrefix(name, "org.apache.cassandra.db.marshal.TupleType") {
+		names := splitJavaCompositeTypes(name, "org.apache.cassandra.db.marshal.TupleType")
 		types := make([]TypeInfo, len(names))
 
 		for i, name := range names {
@@ -199,8 +199,8 @@ func getCassandraLongType(name string, protoVer byte, logger StdLogger) TypeInfo
 			NativeType: NewNativeType(protoVer, TypeTuple),
 			Elems:      types,
 		}
-	} else if strings.HasPrefix(name, UDT_TYPE) {
-		names := splitJavaCompositeTypes(name, UDT_TYPE)
+	} else if strings.HasPrefix(name, "org.apache.cassandra.db.marshal.UserType") {
+		names := splitJavaCompositeTypes(name, "org.apache.cassandra.db.marshal.UserType")
 		fields := make([]UDTField, len(names)-2)
 
 		for i := 2; i < len(names); i++ {
@@ -219,8 +219,8 @@ func getCassandraLongType(name string, protoVer byte, logger StdLogger) TypeInfo
 			Name:       string(udtName),
 			Elements:   fields,
 		}
-	} else if strings.HasPrefix(name, VECTOR_TYPE) {
-		names := splitJavaCompositeTypes(name, VECTOR_TYPE)
+	} else if strings.HasPrefix(name, "org.apache.cassandra.db.marshal.VectorType") {
+		names := splitJavaCompositeTypes(name, "org.apache.cassandra.db.marshal.VectorType")
 		subType := getCassandraLongType(strings.TrimSpace(names[0]), protoVer, logger)
 		dim, err := strconv.Atoi(strings.TrimSpace(names[1]))
 		if err != nil {
@@ -229,7 +229,7 @@ func getCassandraLongType(name string, protoVer byte, logger StdLogger) TypeInfo
 		}
 
 		return VectorType{
-			NativeType: NewCustomType(protoVer, TypeCustom, VECTOR_TYPE),
+			NativeType: NewCustomType(protoVer, TypeCustom, "org.apache.cassandra.db.marshal.VectorType"),
 			SubType:    subType,
 			Dimensions: dim,
 		}
@@ -285,7 +285,7 @@ func getCassandraType(name string, protoVer byte, logger StdLogger) TypeInfo {
 		dim, _ := strconv.Atoi(strings.TrimSpace(names[1]))
 
 		return VectorType{
-			NativeType: NewCustomType(protoVer, TypeCustom, VECTOR_TYPE),
+			NativeType: NewCustomType(protoVer, TypeCustom, "org.apache.cassandra.db.marshal.VectorType"),
 			SubType:    subType,
 			Dimensions: dim,
 		}

helpers_test.go
@@ -234,7 +234,7 @@ func TestGetCassandraType(t *testing.T) {
 			"vector<float, 3>", VectorType{
 				NativeType: NativeType{
 					typ:    TypeCustom,
-					custom: VECTOR_TYPE,
+					custom: "org.apache.cassandra.db.marshal.VectorType",
 				},
 				SubType:    NativeType{typ: TypeFloat},
 				Dimensions: 3,
@@ -244,12 +244,12 @@ func TestGetCassandraType(t *testing.T) {
 			"vector<vector<float, 3>, 5>", VectorType{
 				NativeType: NativeType{
 					typ:    TypeCustom,
-					custom: VECTOR_TYPE,
+					custom: "org.apache.cassandra.db.marshal.VectorType",
 				},
 				SubType: VectorType{
 					NativeType: NativeType{
 						typ:    TypeCustom,
-						custom: VECTOR_TYPE,
+						custom: "org.apache.cassandra.db.marshal.VectorType",
 					},
 					SubType:    NativeType{typ: TypeFloat},
 					Dimensions: 3,
@@ -261,7 +261,7 @@ func TestGetCassandraType(t *testing.T) {
 			"vector<map<uuid,timestamp>, 5>", VectorType{
 				NativeType: NativeType{
 					typ:    TypeCustom,
-					custom: VECTOR_TYPE,
+					custom: "org.apache.cassandra.db.marshal.VectorType",
 				},
 				SubType: CollectionType{
 					NativeType: NativeType{typ: TypeMap},
@@ -275,7 +275,7 @@ func TestGetCassandraType(t *testing.T) {
 			"vector<frozen<tuple<int, float>>, 100>", VectorType{
 				NativeType: NativeType{
 					typ:    TypeCustom,
-					custom: VECTOR_TYPE,
+					custom: "org.apache.cassandra.db.marshal.VectorType",
 				},
 				SubType: TupleTypeInfo{
 					NativeType: NativeType{typ: TypeTuple},

integration_serialization_scylla_test.go
@@ -67,7 +67,7 @@ func checkTypeMarshal(t *testing.T, tc valcases.SimpleTypeCases) {
 	cqlName := tc.CQLName
 	t.Run(cqlName, func(t *testing.T) {
 		tp := Type(tc.CQLType)
-		cqlType := NewNativeType(4, tp, "")
+		cqlType := NewNativeType(4, tp)
 
 		for _, valCase := range tc.Cases {
 			for _, langCase := range valCase.LangCases {
@@ -89,7 +89,7 @@ func checkTypeUnmarshal(t *testing.T, tc valcases.SimpleTypeCases) {
 	cqlName := tc.CQLName
 	t.Run(cqlName, func(t *testing.T) {
 		tp := Type(tc.CQLType)
-		cqlType := NewNativeType(4, tp, "")
+		cqlType := NewNativeType(4, tp)
 
 		for _, valCase := range tc.Cases {
 			for _, langCase := range valCase.LangCases {
@@ -115,7 +115,7 @@ func checkTypeInsertSelect(t *testing.T, session *Session, insertStmt, selectStm
 	cqlName := tc.CQLName
 	t.Run(cqlName, func(t *testing.T) {
 		tp := Type(tc.CQLType)
-		cqlType := NewNativeType(4, tp, "")
+		cqlType := NewNativeType(4, tp)
 
 		for _, valCase := range tc.Cases {
 			valCaseName := valCase.Name

marshal.go
@@ -29,6 +29,7 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"math/bits"
 	"reflect"
 	"unsafe"
 

tests/serialization/marshal_0_unset_test.go
@@ -18,37 +18,37 @@ func TestMarshalUnsetColumn(t *testing.T) {
 		err     bool
 	}
 
-	elem := gocql.NewNativeType(3, gocql.TypeSmallInt, "")
+	elem := gocql.NewNativeType(3, gocql.TypeSmallInt)
 	cases := []tCase{
-		{gocql.NewNativeType(4, gocql.TypeBoolean, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeTinyInt, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeSmallInt, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeInt, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeBigInt, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeCounter, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeVarint, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeFloat, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeDouble, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeDecimal, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeVarchar, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeText, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeBlob, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeAscii, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeUUID, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeTimeUUID, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeInet, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeTime, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeTimestamp, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeDate, ""), true, false},
-		{gocql.NewNativeType(4, gocql.TypeDuration, ""), true, false},
+		{gocql.NewNativeType(4, gocql.TypeBoolean), true, false},
+		{gocql.NewNativeType(4, gocql.TypeTinyInt), true, false},
+		{gocql.NewNativeType(4, gocql.TypeSmallInt), true, false},
+		{gocql.NewNativeType(4, gocql.TypeInt), true, false},
+		{gocql.NewNativeType(4, gocql.TypeBigInt), true, false},
+		{gocql.NewNativeType(4, gocql.TypeCounter), true, false},
+		{gocql.NewNativeType(4, gocql.TypeVarint), true, false},
+		{gocql.NewNativeType(4, gocql.TypeFloat), true, false},
+		{gocql.NewNativeType(4, gocql.TypeDouble), true, false},
+		{gocql.NewNativeType(4, gocql.TypeDecimal), true, false},
+		{gocql.NewNativeType(4, gocql.TypeVarchar), true, false},
+		{gocql.NewNativeType(4, gocql.TypeText), true, false},
+		{gocql.NewNativeType(4, gocql.TypeBlob), true, false},
+		{gocql.NewNativeType(4, gocql.TypeAscii), true, false},
+		{gocql.NewNativeType(4, gocql.TypeUUID), true, false},
+		{gocql.NewNativeType(4, gocql.TypeTimeUUID), true, false},
+		{gocql.NewNativeType(4, gocql.TypeInet), true, false},
+		{gocql.NewNativeType(4, gocql.TypeTime), true, false},
+		{gocql.NewNativeType(4, gocql.TypeTimestamp), true, false},
+		{gocql.NewNativeType(4, gocql.TypeDate), true, false},
+		{gocql.NewNativeType(4, gocql.TypeDuration), true, false},
 
-		{gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeList, ""), nil, elem), true, false},
-		{gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeSet, ""), nil, elem), true, false},
+		{gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeList), nil, elem), true, false},
+		{gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeSet), nil, elem), true, false},
 
-		{gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeMap, ""), elem, elem), true, false},
+		{gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeMap), elem, elem), true, false},
 
 		{gocql.NewUDTType(3, "udt1", "", gocql.UDTField{Name: "1", Type: elem}), true, true},
-		{gocql.NewTupleType(gocql.NewNativeType(3, gocql.TypeTuple, ""), elem), true, true},
+		{gocql.NewTupleType(gocql.NewNativeType(3, gocql.TypeTuple), elem), true, true},
 	}
 
 	for _, expected := range cases {

tests/serialization/marshal_10_decimal_corrupt_test.go
@@ -18,7 +18,7 @@ func TestMarshalDecimalCorrupt(t *testing.T) {
 		name      string
 	}
 
-	tType := gocql.NewNativeType(4, gocql.TypeDecimal, "")
+	tType := gocql.NewNativeType(4, gocql.TypeDecimal)
 
 	testSuites := [2]testSuite{
 		{

commit 6be221eaaf428b9d6eed695f575457234c82c46a

tests: fix vector collections and bench marshal types
The vector tests were fixed:
1. They work on 2025.4, so enabled some that weren't before.
It's far less interesting to test them on lower versions of ScyllaDB
2. Some were inaccurate (set is returned sorted, but the tests did not account for it, so failed)
and were fixed.

They partially revert baffd806386d62a699edd2842d872ee1903bcbbd which disabled them, and they now all pass.

bench_marshal_test.go was broken since c7ad64d7a6b36d6159ee81c9d72e2ba338b25951 and was now amended.

Signed-off-by: Yaniv Kaul <yaniv.kaul@scylladb.com>

tests/bench/bench_marshal_test.go
@@ -59,7 +59,7 @@ func generateRandomJSON(size int) string {
 func BenchmarkSerialization(b *testing.B) {
 	b.Run("SimpleTypes", func(b *testing.B) {
 		b.Run("Int", func(b *testing.B) {
-			tType := gocql.NewNativeType(4, gocql.TypeInt, "")
+			tType := gocql.NewNativeType(4, gocql.TypeInt)
 			var val int = 42
 			b.Run("Marshal", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
@@ -95,7 +95,7 @@ func BenchmarkSerialization(b *testing.B) {
 
 		for _, c := range cases {
 			b.Run("Blob"+c.name, func(b *testing.B) {
-				tType := gocql.NewNativeType(4, gocql.TypeBlob, "")
+				tType := gocql.NewNativeType(4, gocql.TypeBlob)
 				val := generateRandomBinaryData(c.size)
 				b.Run("Marshal", func(b *testing.B) {
 					for i := 0; i < b.N; i++ {
@@ -123,7 +123,7 @@ func BenchmarkSerialization(b *testing.B) {
 
 		for _, c := range cases {
 			b.Run("Text"+c.name, func(b *testing.B) {
-				tType := gocql.NewNativeType(4, gocql.TypeText, "")
+				tType := gocql.NewNativeType(4, gocql.TypeText)
 				val := generateRandomJSON(c.size)
 				b.Run("Marshal", func(b *testing.B) {
 					for i := 0; i < b.N; i++ {
@@ -150,7 +150,7 @@ func BenchmarkSerialization(b *testing.B) {
 		}
 
 		b.Run("UUID", func(b *testing.B) {
-			tType := gocql.NewNativeType(4, gocql.TypeUUID, "")
+			tType := gocql.NewNativeType(4, gocql.TypeUUID)
 			val := gocql.UUID{}
 			b.Run("Marshal", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
@@ -176,7 +176,7 @@ func BenchmarkSerialization(b *testing.B) {
 		})
 
 		b.Run("Duration", func(b *testing.B) {
-			tType := gocql.NewNativeType(4, gocql.TypeDuration, "")
+			tType := gocql.NewNativeType(4, gocql.TypeDuration)
 			val := gocql.Duration{Nanoseconds: 300000000000}
 			b.Run("Marshal", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
@@ -202,7 +202,7 @@ func BenchmarkSerialization(b *testing.B) {
 		})
 
 		b.Run("Timestamp", func(b *testing.B) {
-			tType := gocql.NewNativeType(4, gocql.TypeTimestamp, "")
+			tType := gocql.NewNativeType(4, gocql.TypeTimestamp)
 			val := time.Now()
 			b.Run("Marshal", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
@@ -231,8 +231,8 @@ func BenchmarkSerialization(b *testing.B) {
 	b.Run("ComplexTypes", func(b *testing.B) {
 		b.Run("List", func(b *testing.B) {
 			tType := gocql.CollectionType{
-				NativeType: gocql.NewNativeType(4, gocql.TypeList, ""),
-				Elem:       gocql.NewNativeType(4, gocql.TypeText, ""),
+				NativeType: gocql.NewNativeType(4, gocql.TypeList),
+				Elem:       gocql.NewNativeType(4, gocql.TypeText),
 			}
 			val := []string{"foo", "bar", "baz"}
 			b.Run("Marshal", func(b *testing.B) {
@@ -260,9 +260,9 @@ func BenchmarkSerialization(b *testing.B) {
 
 		b.Run("Map", func(b *testing.B) {
 			tType := gocql.CollectionType{
-				NativeType: gocql.NewNativeType(4, gocql.TypeMap, ""),
-				Key:        gocql.NewNativeType(4, gocql.TypeVarchar, ""),
-				Elem:       gocql.NewNativeType(4, gocql.TypeInt, ""),
+				NativeType: gocql.NewNativeType(4, gocql.TypeMap),
+				Key:        gocql.NewNativeType(4, gocql.TypeVarchar),
+				Elem:       gocql.NewNativeType(4, gocql.TypeInt),
 			}
 			val := map[string]int{"a": 1, "b": 2}
 			b.Run("Marshal", func(b *testing.B) {
@@ -290,8 +290,8 @@ func BenchmarkSerialization(b *testing.B) {
 
 		b.Run("Set", func(b *testing.B) {
 			tType := gocql.CollectionType{
-				NativeType: gocql.NewNativeType(4, gocql.TypeSet, ""),
-				Elem:       gocql.NewNativeType(4, gocql.TypeInt, ""),
+				NativeType: gocql.NewNativeType(4, gocql.TypeSet),
+				Elem:       gocql.NewNativeType(4, gocql.TypeInt),
 			}
 			val := map[int]struct{}{1: {}, 2: {}}
 			b.Run("Marshal", func(b *testing.B) {
@@ -331,21 +331,21 @@ func BenchmarkSerialization(b *testing.B) {
 			}
 
 			tType := gocql.UDTTypeInfo{
-				NativeType: gocql.NewNativeType(4, gocql.TypeUDT, ""),
+				NativeType: gocql.NewNativeType(4, gocql.TypeUDT),
 				Name:       "myudt",
 				KeySpace:   "myks",
 				Elements: []gocql.UDTField{
 					{
 						Name: "id",
-						Type: gocql.NewNativeType(4, gocql.TypeUUID, ""),
+						Type: gocql.NewNativeType(4, gocql.TypeUUID),
 					},
 					{
 						Name: "name",
-						Type: gocql.NewNativeType(4, gocql.TypeText, ""),
+						Type: gocql.NewNativeType(4, gocql.TypeText),
 					},
 					{
 						Name: "value",
-						Type: gocql.NewNativeType(4, gocql.TypeInt, ""),
+						Type: gocql.NewNativeType(4, gocql.TypeInt),
 					},
 				},
 			}
@@ -383,10 +383,10 @@ func BenchmarkSerialization(b *testing.B) {
 			}
 
 			tType := gocql.TupleTypeInfo{
-				NativeType: gocql.NewNativeType(4, gocql.TypeTuple, ""),
+				NativeType: gocql.NewNativeType(4, gocql.TypeTuple),
 				Elems: []gocql.TypeInfo{
-					gocql.NewNativeType(4, gocql.TypeInt, ""),
-					gocql.NewNativeType(4, gocql.TypeText, ""),
+					gocql.NewNativeType(4, gocql.TypeInt),
+					gocql.NewNativeType(4, gocql.TypeText),
 				},
 			}
 			b.Run("Marshal", func(b *testing.B) {
@@ -435,26 +435,26 @@ func BenchmarkSerialization(b *testing.B) {
 			}
 
 			tType := gocql.CollectionType{
-				NativeType: gocql.NewNativeType(4, gocql.TypeList, ""),
+				NativeType: gocql.NewNativeType(4, gocql.TypeList),
 				Elem: gocql.CollectionType{
-					NativeType: gocql.NewNativeType(4, gocql.TypeMap, ""),
-					Key:        gocql.NewNativeType(4, gocql.TypeText, ""),
+					NativeType: gocql.NewNativeType(4, gocql.TypeMap),
+					Key:        gocql.NewNativeType(4, gocql.TypeText),
 					Elem: gocql.UDTTypeInfo{
-						NativeType: gocql.NewNativeType(4, gocql.TypeUDT, ""),
+						NativeType: gocql.NewNativeType(4, gocql.TypeUDT),
 						Name:       "myudt",
 						KeySpace:   "myks",
 						Elements: []gocql.UDTField{
 							{
 								Name: "id",
-								Type: gocql.NewNativeType(4, gocql.TypeUUID, ""),
+								Type: gocql.NewNativeType(4, gocql.TypeUUID),
 							},
 							{
 								Name: "name",
-								Type: gocql.NewNativeType(4, gocql.TypeText, ""),
+								Type: gocql.NewNativeType(4, gocql.TypeText),
 							},
 							{
 								Name: "value",
-								Type: gocql.NewNativeType(4, gocql.TypeInt, ""),
+								Type: gocql.NewNativeType(4, gocql.TypeInt),
 							},
 						},
 					},

vector_test.go
@@ -31,6 +31,7 @@ import (
 	"fmt"
 	"net"
 	"reflect"
+	"sort"
 	"testing"
 	"time"
 
@@ -106,8 +107,8 @@ func TestVector_Types(t *testing.T) {
 		t.Skip("Vector types have been introduced in Cassandra 5.0")
 	}
 
-	if *flagDistribution == "scylla" && flagCassVersion.Before(2025, 3, 0) {
-		t.Skip("Vector types have been introduced in ScyllaDB 2025.3")
+	if *flagDistribution == "scylla" && flagCassVersion.Before(2025, 4, 0) {
+		t.Skip("Vector types are useful in ScyllaDB from 2025.4 and on")
 	}
 
 	timestamp1, _ := time.Parse("2006-01-02", "2000-01-01")
@@ -126,13 +127,13 @@ func TestVector_Types(t *testing.T) {
 	duration2 := Duration{1, 1, 1920000000000}
 	duration3 := Duration{31, 0, 60000000000}
 
-	// map1 := make(map[string]int)
-	// map1["a"] = 1
-	// map1["b"] = 2
-	// map1["c"] = 3
-	// map2 := make(map[string]int)
-	// map2["abc"] = 123
-	// map3 := make(map[string]int)
+	map1 := make(map[string]int)
+	map1["a"] = 1
+	map1["b"] = 2
+	map1["c"] = 3
+	map2 := make(map[string]int)
+	map2["abc"] = 123
+	map3 := make(map[string]int)
 
 	testCases := []struct {
 		name       string
@@ -174,16 +175,53 @@ func TestVector_Types(t *testing.T) {
 		{name: "tinyint", cqlType: TypeTinyInt.String(), value: []int8{127, 9, -123}},
 		{name: "duration", cqlType: TypeDuration.String(), value: []Duration{duration1, duration2, duration3}},
 		{name: "vector_vector_float", cqlType: "vector<float, 5>", value: [][]float32{{0.1, -1.2, 3, 5, 5}, {10.1, -122222.0002, 35.0, 1, 1}, {0, 0, 0, 0, 0}}},
-		// {name: "vector_vector_set_float", cqlType: "vector<set<float>, 5>", value: [][][]float32{
-		// 	{{1, 2}, {2, -1}, {3}, {0}, {-1.3}},
-		// 	{{2, 3}, {2, -1}, {3}, {0}, {-1.3}},
-		// 	{{1, 1000.0}, {0}, {}, {12, 14, 15, 16}, {-1.3}},
-		// }}, // disable until INSERTing Vector<collection> is fixed on scylladb side
+		{
+			name:    "vector_vector_set_float",
+			cqlType: "vector<set<float>, 5>",
+			value: [][][]float32{
+				{{1, 2}, {2, -1}, {3}, {0}, {-1.3}},
+				{{2, 3}, {2, -1}, {3}, {0}, {-1.3}},
+				{{1, 1000.0}, {0}, {}, {12, 14, 15, 16}, {-1.3}},
+			},
+			comparator: func(e interface{}, a interface{}) {
+				expected := e.([][][]float32)
+				actual := a.([][][]float32)
+				tests.AssertEqual(t, "vector size", len(expected), len(actual))
+				for i := range expected {
+					expVector := expected[i]
+					actVector := actual[i]
+					tests.AssertEqual(t, "vector size", len(expVector), len(actVector))
+					for j := range expVector {
+						expSet := append([]float32(nil), expVector[j]...)
+						actSet := append([]float32(nil), actVector[j]...)
+						sort.Slice(expSet, func(a, b int) bool { return expSet[a] < expSet[b] })
+						sort.Slice(actSet, func(a, b int) bool { return actSet[a] < actSet[b] })
+						tests.AssertDeepEqual(t, "vector set", expSet, actSet)
+					}
+				}
+			},
+		},
 		{name: "vector_tuple_text_int_float", cqlType: "tuple<text, int, float>", value: [][]interface{}{{"a", 1, float32(0.5)}, {"b", 2, float32(-1.2)}, {"c", 3, float32(0)}}},
-		{name: "vector_tuple_text_list_text", cqlType: "tuple<text, list<text>>", value: [][]interface{}{{"a", []string{"b", "c"}}, {"d", []string{"e", "f", "g"}}, {"h", []string{"i"}}}},
-		// {name: "vector_set_text", cqlType: "set<text>", value: [][]string{{"a", "b"}, {"c", "d"}, {"e", "f"}}}, // disable until INSERTing Vector<collection> is fixed on scylladb side
+		{name: "vector_tuple_text_list_text", cqlType: "tuple<text, list<text>>", value: [][]interface{}{{"a", []string{"b", "c"}}, {"d", []string{"e", "g", "f"}}, {"h", []string{"i"}}}},
+		{
+			name:    "vector_set_text",
+			cqlType: "set<text>",
+			value:   [][]string{{"a", "b"}, {"c", "d"}, {"f", "e"}},
+			comparator: func(e interface{}, a interface{}) {
+				expected := e.([][]string)
+				actual := a.([][]string)
+				tests.AssertEqual(t, "vector size", len(expected), len(actual))
+				for i := range expected {
+					expSet := append([]string(nil), expected[i]...)
+					actSet := append([]string(nil), actual[i]...)
+					sort.Strings(expSet)
+					sort.Strings(actSet)
+					tests.AssertDeepEqual(t, "vector set", expSet, actSet)
+				}
+			},
+		},
 		{name: "vector_list_int", cqlType: "list<int>", value: [][]int32{{1, 2, 3}, {-1, -2, -3}, {0, 0, 0}}},
-		// {name: "vector_map_text_int", cqlType: "map<text, int>", value: []map[string]int{map1, map2, map3}}, // disable until INSERTing Vector<collection> is fixed on scylladb side
+		{name: "vector_map_text_int", cqlType: "map<text, int>", value: []map[string]int{map1, map2, map3}},
 	}
 
 	for _, test := range testCases {
@@ -331,14 +369,17 @@ func TestVector_MissingDimension(t *testing.T) {
 }
 
 func TestVector_SubTypeParsing(t *testing.T) {
+
+	if *flagDistribution == "scylla" && flagCassVersion.Before(2025, 4, 0) {
+		t.Skip("Vector types are useful in ScyllaDB from 2025.4 and on")
+	}
 	testCases := []struct {
 		name     string
 		custom   string
 		expected TypeInfo
 	}{
 		{name: "text", custom: "org.apache.cassandra.db.marshal.UTF8Type", expected: NativeType{typ: TypeVarchar}},
-		// disable until INSERTing Vector<collection> is fixed on scylladb side
-		// {name: "set_int", custom: "org.apache.cassandra.db.marshal.SetType(org.apache.cassandra.db.marshal.Int32Type)", expected: CollectionType{NativeType{typ: TypeSet}, nil, NativeType{typ: TypeInt}}},
+		{name: "set_int", custom: "org.apache.cassandra.db.marshal.SetType(org.apache.cassandra.db.marshal.Int32Type)", expected: CollectionType{NativeType: NativeType{typ: TypeSet}, Key: nil, Elem: NativeType{typ: TypeInt}}},
 		{
 			name:   "udt",
 			custom: "org.apache.cassandra.db.marshal.UserType(gocql_test,706572736f6e,66697273745f6e616d65:org.apache.cassandra.db.marshal.UTF8Type,6c6173745f6e616d65:org.apache.cassandra.db.marshal.UTF8Type,616765:org.apache.cassandra.db.marshal.Int32Type)",
@@ -391,24 +432,23 @@ func TestVector_SubTypeParsing(t *testing.T) {
 				},
 			},
 		},
-		// disable until INSERTing Vector<collection> is fixed on scylladb side
-		// {
-		// 	name:   "set_map_vector_text_text",
-		// 	custom: "org.apache.cassandra.db.marshal.SetType(org.apache.cassandra.db.marshal.MapType(org.apache.cassandra.db.marshal.VectorType(org.apache.cassandra.db.marshal.Int32Type, 10),org.apache.cassandra.db.marshal.UTF8Type))",
-		// 	expected: CollectionType{
-		// 		NativeType: NativeType{typ: TypeSet},
-		// 		Key:        nil,
-		// 		Elem: CollectionType{
-		// 			NativeType{typ: TypeMap},
-		// 			VectorType{
-		// 				NativeType: NativeType{typ: TypeCustom, custom: "org.apache.cassandra.db.marshal.VectorType"},
-		// 				SubType:    NativeType{typ: TypeInt},
-		// 				Dimensions: 10,
-		// 			},
-		// 			NativeType{typ: TypeVarchar},
-		// 		},
-		// 	},
-		// },
+		{
+			name:   "set_map_vector_text_text",
+			custom: "org.apache.cassandra.db.marshal.SetType(org.apache.cassandra.db.marshal.MapType(org.apache.cassandra.db.marshal.VectorType(org.apache.cassandra.db.marshal.Int32Type, 10),org.apache.cassandra.db.marshal.UTF8Type))",
+			expected: CollectionType{
+				NativeType: NativeType{typ: TypeSet},
+				Key:        nil,
+				Elem: CollectionType{
+					NativeType: NativeType{typ: TypeMap},
+					Key: VectorType{
+						NativeType: NativeType{typ: TypeCustom, custom: "org.apache.cassandra.db.marshal.VectorType"},
+						SubType:    NativeType{typ: TypeInt},
+						Dimensions: 10,
+					},
+					Elem: NativeType{typ: TypeVarchar},
+				},
+			},
+		},
 	}
 
 	for _, test := range testCases {